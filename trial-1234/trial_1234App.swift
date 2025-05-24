df743bb2edca979d674128c87748c658393705b5
AbhinavAnand241201 <iabhinavanand10@gmail.com>
2025-05-24 19:48:10 +0530

    Initial Commit

---
+++ trial-1234/trial_1234App.swift
@@ -0,0 +1,17 @@
+//
+//  trial_1234App.swift
+//  trial-1234
+//
+//  Created by ABHINAV ANAND  on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct trial_1234App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
